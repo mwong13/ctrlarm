26ff3da0223a4da6485978c26f1615108e7b6a08
Michael Wong <miwong@microsoft.com>
2020-02-06 00:53:25 +0000

    kubebuilder create api

--- main.go
+++ main.go
@@ -76,6 +76,14 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "ManagedCluster")
 		os.Exit(1)
 	}
+	if err = (&controllers.ResourceGroupReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("ResourceGroup"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ResourceGroup")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
